77d3797f1e1fb6b3534bd8333467c54e25fa7ed5
Manohari <sankarmano1998@gmail.com>
2025-09-13 21:27:42 +0530

    portfolio

--- ReactJs/Portfolio/react-portfolio/data/projectData.js
+++ ReactJs/Portfolio/react-portfolio/data/projectData.js
@@ -10,38 +10,38 @@ const projectData = [
     id : 1, 
     projectImage : youtubehero, 
     projectName : "YouTube Clone" , 
-    demoLink:"https://spectacular-faun-0bc2bd.netlify.app/" , 
+    demoLink:"https://youtube-clonehtml.netlify.app/" , 
     sourceLink :"https://github.com/SManohari/MyProjects/tree/main/HTML_CSS/YouTube_Clone"
   }, 
   { 
     id : 2, 
     projectImage : todo, 
     projectName : "To Do List" ,
-    demoLink:"https://polite-moonbeam-f4f6fa.netlify.app/" , 
+    demoLink:"https://jstodoo-list.netlify.app/" , 
     sourceLink :"https://github.com/SManohari/MyProjects/tree/main/JavaScript/Todo-List"
   }, 
   {  id : 3,
      projectImage : amazon, 
      projectName : "Amazon Clone" ,
-     demoLink:"https://startling-vacherin-3e6711.netlify.app/amazon" , 
+     demoLink:"https://jsamazon-clone.netlify.app/" , 
      sourceLink :"https://github.com/SManohari/MyProjects/tree/main/JavaScript/Amazon-clone"
     },
   { id : 4, 
     projectImage : rockpaper, 
     projectName : "Rock Paper Scissor" ,
-    demoLink:"https://voluble-medovik-fda340.netlify.app/" , 
+    demoLink:"https://jsrock-paper-scissor.netlify.app/" , 
     sourceLink :"https://github.com/SManohari/MyProjects/tree/main/JavaScript/Rock-Paper-Scissors"
   }, 
   {  id : 5,
      projectImage : TodoReact, 
      projectName : "To Do List - React" ,
-     demoLink:"" , 
+     demoLink:"https://reactjs-todo-list-app-proj.netlify.app/" , 
      sourceLink :"https://github.com/SManohari/MyProjects/tree/main/ReactJs/Todo-list"
     },
   { id : 6, 
     projectImage : keeperapp,
     projectName : "Keeper App" ,
-    demoLink:"" ,
+    demoLink:"https://reactjs-keeper-app-proj.netlify.app/" ,
     sourceLink :"https://github.com/SManohari/MyProjects/tree/main/ReactJs/keeper-app"
     }
    ] 
